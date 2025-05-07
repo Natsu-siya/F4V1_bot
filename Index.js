022392345b521806ddc9d54dddb286f65820154b
Sheikh Alvi <jannatfaiza098@gmail.com>
2025-05-07 21:52:32 +0600

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,9 @@
+const config = require('./config');
+
+console.log(`Bot Name: ${config.botName}`);
+console.log(`Owner: ${config.ownerName}`);
+console.log(`Language: ${config.language}`);
+console.log(`Prefix: ${config.prefix}`);
+console.log(`Version: ${config.version}`);
+
+console.log(`\n${config.botName} is now running...`);
