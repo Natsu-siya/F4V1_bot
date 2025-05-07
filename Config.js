0020880891e7d4bee84d8889596a11754c0d9c9d
Sheikh Alvi <jannatfaiza098@gmail.com>
2025-05-07 21:49:51 +0600

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,10 @@
+const config = {
+  botName: "F4V1_bot",
+  ownerName: "Sheikh Alvi",
+  prefix: "/",
+  language: "en",
+  version: "1.0.0",
+  supportContact: "https://github.com/Natsu-siya"
+};
+
+module.exports = config;
